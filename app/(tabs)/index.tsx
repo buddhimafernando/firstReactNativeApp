4de4836c09a48e83823ff46b0772eb1e46a9c732
Buddhima Fernando <harun.fernando.hf@gmail.com>
2025-04-05 23:36:46 +0530

    Add VSCode settings and update title in HomeScreen component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">This is my first project!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
