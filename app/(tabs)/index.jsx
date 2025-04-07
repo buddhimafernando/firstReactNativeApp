0e2ea06b5adb612487e11bd05e63d21b4b2f2bb3
Buddhima Fernando <harun.fernando.hf@gmail.com>
2025-04-07 20:01:02 +0530

    Update app icon and splash screen images; add Coffee shop component

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,10 +1,17 @@
 import { View, Text, StyleSheet, ImageBackground} from 'react-native'
 import React from 'react'
 
+import icedCoffeeImg from "@/assets/images/iced-coffee.png"
+
 const app = () => {
   return (
     <View style={styles.container}>
+      <ImageBackground 
+        source = {icedCoffeeImg}
+        resizeMode='cover'
+        style={styles.image}>
       <Text style = {styles.text}>Coffee shop</Text>
+      </ImageBackground>
     </View>
   )
 }
@@ -16,10 +23,18 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column'
   },
+  image:{
+    width: '100%',
+    height:"100%",
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
+  },
   text:{
     color: 'white',
     fontSize: 42,
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)'
   }
 })
